b94a9afab1d42b738bf5b1611c0f16dadd0de18b
ashrafwbst <er.ashrafkamal@gmail.com>
2020-09-25 16:28:35 +0530

    added functions and API for pushing booking

--- database/migrations/20200907124311_unitTypeV2.js
+++ database/migrations/20200907124311_unitTypeV2.js
@@ -29,6 +29,7 @@ exports.up = (knex) => knex.schema.createTable(TABLE_NAME, (table) => {
   table.json('languages');
   table.string('website');
   table.string('image');
+  table.boolean('isChannelManagerActivated').defaultTo(false);
   table.timestamp('created_at').notNull().defaultTo(knex.raw('CURRENT_TIMESTAMP'));
   table.timestamp('updated_at').notNull().defaultTo(knex.raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
   table.foreign('userId').references('users.id').onUpdate('CASCADE').onDelete('CASCADE');
--- src/channelManagement/index.js
+++ src/channelManagement/index.js
@@ -5,14 +5,17 @@ const axios = require('axios');
 const getConfig = require('../channexIntegration/config');
 const auth = require('../channexIntegration/authorization');
 
-// This function will update the rate on channex
+const user = Config.get('CHANNEX_USER');
+const password = Config.get('CHANNEX_PASSWORD');
+
+/**  This function will update the rate on channex
 /** @param propertyId String
  *  @param ratePlanId String
  *  @param date format of date YYYY-MM-DD
+ *  @returns success message with 200 code(if entities are correct)
+ *  TODO => put the function in try catch block
  */
 const updateRate = async (propertyId, ratePlanId, dateFrom, dateTo, rate) => {
-  const user = Config.get('CHANNEX_USER');
-  const password = Config.get('CHANNEX_PASSWORD');
   const token = await auth(user, password);
   const payload = {
     values: [{
@@ -31,9 +34,46 @@ const updateRate = async (propertyId, ratePlanId, dateFrom, dateTo, rate) => {
 
 // This function will push booking to channex
 /**
- * TODO push booking to channex with dyanmic data coming from API
+ * @param ratePlanId String
+ * @param bookingData Object of booking deatils
+ * @returns object with booking data
  */
-// const pushBooking = () => {
-
-// };
-module.exports = updateRate;
+const pushBooking = async (ratePlanId, bookingData) => {
+  const token = await auth(user, password);
+  const payload = {
+    booking: {
+      status: 'new',
+      channel_id: '6496cbb9-f4d4-48ea-ab47-ff253c6163e9',
+      reservation_id: bookingData.id,
+      arrival_date: bookingData.startDate,
+      departure_date: bookingData.endDate,
+      currency: 'EUR',
+      customer: {
+        name: bookingData.guest,
+      },
+      rooms: [
+        {
+          occupancy: {
+            adults: bookingData.adult,
+            children: bookingData.children,
+            infants: bookingData.infants,
+          },
+          rate_plan_id: ratePlanId,
+          days: bookingData.days,
+        },
+      ],
+    },
+  };
+  const config = getConfig('post', 'bookings/push', token, payload);
+  console.log('push booking config', config);
+  return true;
+  /**
+   * TODO uncomment the below lines and return the required data
+   */
+  // const res = await axios(config);
+  // return res;
+};
+module.exports = {
+  updateRate,
+  pushBooking,
+};
