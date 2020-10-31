4d977814b1c73b22b6be5bcf1dee52558b1e98b6
hashimwbst <hashimwebsul@gmail.com>
2020-10-31 15:41:44 +0530

    testing for invoice

--- src/invoiceTemplate/invoiceTemplate.js
+++ src/invoiceTemplate/invoiceTemplate.js
@@ -93,8 +93,8 @@ ${`${website || 'N/A'}`}
 
 
 
-<div class="content" style="margin: 0 auto;padding: 0px 40px;
-padding-top: 20px; max-width: 700px;display: block; ">
+<div style="margin: 0 auto;padding: 0px 40px; padding-top: 20px;
+max-width: 700px;display: block;">
 <table style="width: 100%;color: #666; border-bottom: 1px solid #333;">
 <tr>
 <td>
@@ -111,74 +111,74 @@ CLIENT</span>
 </div>
 
 
-<div class=" content" style="margin: 0 auto;padding: 0px 20px; padding-top: 10px;
+<div class=" content" style="margin: 0 auto;padding: 0px 40px; padding-top: 10px;
 max-width: 700px;display: block;">
 <table style="width: 100%;color: #666">
 <tr>
-<td style="padding: 0px 10px;">
+<td>
 <span style="color: rgba(62, 63, 66, 0.5);font-size: 9px;">
 DATE/TIME</span>
 </td>
-<td align="right" style="padding: 0px 10px;">
+<td align="right">
 <span style="color: #3E3F42;font-size: 9px;">
 ${`${date || 'N/A'}`} /${`${time || 'N/A'}`}
 </span>
 </td>
 
-<td style="padding: 0px 10px;">
+<td>
 <span style="color: rgba(62, 63, 66, 0.5);
 font-size: 9px;">FULL
 NAME</span>
 </td>
-<td align="right" style="padding: 0px 9px;">
+<td align="right">
 <span style="color: #3E3F42;font-size: 9px;">${`${clientName}`}</span>
 </td>
 </tr>
 
 
 <tr>
-<td style="padding: 0px 10px;">
+<td>
 <span style="color: rgba(62, 63, 66, 0.5);
 font-size: 9px;">DELIVERY
 DATE</span>
 </td>
-<td align="right" style="padding: 0px 10px;">
+<td align="right">
 <span style="color: #3E3F42;font-size: 9px;">${`${deliveryDate}`}</span>
 </td>
 
-<td style="padding: 0px 10px;">
+<td>
 <span style="color: rgba(62, 63, 66, 0.5);
 font-size: 9px;">EMAIL</span>
 </td>
-<td align="right" style="padding: 0px 9px;">
+<td align="right">
 <span style="color: #3E3F42;font-size: 10px;">${`${email}`}</span>
 </td>
 </tr>
 
 
 <tr>
-<td style="padding: 0px 10px;">
+<td>
 <span style="color: rgba(62, 63, 66, 0.5);
 font-size: 9px;">
 DATE
 </span>
 </td>
-<td align="right" style="padding: 0px 10px;">
+<td align="right">
 <span style="color: #3E3F42;font-size: 9px;">${`${dueDate}`}</span>
 </td>
 
-<td style="padding: 0px 10px;">
+<td>
 <span style="color: rgba(62, 63, 66, 0.5);font-size: 9px;">
 ADDRESS</span>
 </td>
-<td align="right" style="padding: 0px 10px;">
+<td align="right">
 <span style="color: #3E3F42;font-size: 9px;">${`${address || 'N/A'}`}</span>
 </td>
 </tr>
 
 
 <tr>
-<td style="padding: 0px 10px;">
+<td>
 <span style="color: rgba(62, 63, 66, 0.5);font-size: 9px;">PAYMENT
 TYPE</span>
 </td>
@@ -186,11 +186,11 @@ TYPE</span>
 <span style="color: #3E3F42;font-size: 10px;">${`${paymentType || 'N/A'}`}</span>
 </td>
 
-<td style="padding: 0px 10px;">
+<td>
 <span style="color: rgba(62, 63, 66, 0.5);font-size: 9px;">VAT
 ID</span>
 </td>
-<td align="right" style="padding: 0px 10px;">
+<td align="right">
 <span style="color: #3E3F42;font-size: 9px;">
 ${`${vat || 'N/A'}`}</span>
 </td>
@@ -201,28 +201,28 @@ ${`${vat || 'N/A'}`}</span>
 
 
 
-<div class="content" style="margin: 0 auto;padding: 0px 40px;
-padding-top: 60px; max-width: 700px;display: block; ">
+<div class="content" style="margin: 0 auto;padding: 0px 40px; padding-top: 60px;
+max-width: 700px;display: block;">
 <table style="width: 100%;color: #666;border-collapse: collapse;">
 <thead>
 <tr>
-<th style="padding:0px 5px; color: rgba(62, 63, 66, 0.5);
+<th style=" color: rgba(62, 63, 66, 0.5);
 font-size: 9px;text-align: left;
 border-bottom: 1px solid rgba(62, 63, 66, 0.5);font-weight: 100;">ITEM DESCRIPTION</th>
-<th style="padding:0px 5px; color: rgba(62, 63, 66, 0.5);
+<th style="color: rgba(62, 63, 66, 0.5);
 font-size: 9px;text-align: left;
 border-bottom: 1px solid rgba(62, 63, 66, 0.5);font-weight: 100;">QTY</th>
-<th style="padding:0px 5px; color: rgba(62, 63, 66, 0.5);
+<th style="color: rgba(62, 63, 66, 0.5);
 font-size: 9px;text-align: left;
 border-bottom: 1px solid rgba(62, 63, 66, 0.5);font-weight: 100;">PRICE</th>
-<th style="padding:0px 5px; color: rgba(62, 63, 66, 0.5);
+<th style=" color: rgba(62, 63, 66, 0.5);
 font-size:9px;text-align: left;
 border-bottom: 1px solid rgba(62, 63, 66, 0.5);font-weight: 100;">AMOUNT</th>
 
-<th style="padding:0px 5px; color: rgba(62, 63, 66, 0.5);
+<th style="color: rgba(62, 63, 66, 0.5);
 font-size: 9px;text-align: left;
 border-bottom: 1px solid rgba(62, 63, 66, 0.5);font-weight: 100;">DISCOUNT</th>
-<th style="padding:0px 5px; color: rgba(62, 63, 66, 0.5);
+<th style="color: rgba(62, 63, 66, 0.5);
 font-size: 9px;text-align: left;
 border-bottom: 1px solid rgba(62, 63, 66, 0.5);font-weight: 100;">TOTAL</th>
 </tr>
@@ -233,29 +233,29 @@ ${itemData.map((el) => {
     console.log('i');
     return `
 <tr>
-<td style="padding:0px 5px;">
+<td>
 <span style="color: #3E3F42; font-size: 9px;">
 ${`${el.itemDescription}`}</span>
 </td>
-<td style="padding:0px 5px;">
+<td>
 <span style="color: #3E3F42; font-size: 9px;">
 ${`${el.itemQuantity}`}</span>
 </td>
-<td style="padding:0px 5px;">
+<td>
 <span style="color: #3E3F42; font-size: 9px;">
 ${`${el.itemPrice}`} ${`${currency}`}</span>
 </td>
-<td style="padding:0px 5px;">
+<td>
 <span style="color: #3E3F42; font-size: 9px;">
 ${`${el.itemAmount}`} ${`${currency}`}</span>
 </td>
 
 
-<td style="padding:0px 5px;">
+<td>
 <span style="color: #3E3F42; font-size: 9px;">
 ${`${el.itemDiscount}`} ${`${currency}`}</span>
 </td>
-<td style="padding:0px 5px;">
+<td>
 <span style="color: #3E3F42; font-size: 9px;">
 ${`${el.itemTotal}`} ${`${currency}`}</span>
 </td>
@@ -266,12 +266,12 @@ ${`${el.itemTotal}`} ${`${currency}`}</span>
 </tbody>
 </table>
 </div>
-<div class="content" style="margin: 0 auto;padding: 0px 40px;
-padding-top: 30px; max-width: 700px;display: block; ">
+<div style="margin: 0 auto;padding: 0px 40px; padding-top: 30px;
+max-width: 700px;display: block;">
 <table style="width: 100%;color: #666;">
 <tr>
 <td align="right">
-<span style="color: #3E3F42; font-size: 14px; font-weight: 600">
+<span style="color: #3E3F42; font-size: 14px; font-weight: 500">
 Total: ${`${total}`}
 ${`${currency}`}</span>
 </td>
@@ -295,7 +295,7 @@ ${`${currency}`}</span>
 <td class="header container" bgcolor="#ffffff" style="margin: 0 auto!important;
 padding: 0;display: block!important;max-width: 700px!important;clear: both!important;margin-top: 50px;">
 
-<div class="content" style="margin: 0 auto;padding: 80px 40px;max-width: 700px;display: block;">
+<div class="content" style="margin: 0 auto;padding: 80px 40px;max-width: 700px;display: block;" >
 <table style="margin: 0;padding: 0;width: 100%;">
 <tr style="margin: 0;padding: 0;">
 <td align="left" style="margin: 0;padding: 0;">
